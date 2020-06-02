b5f42f9079d9427d2422bf9615d4bb479bd1b01e
root <root@DESKTOP-58DIR6U.localdomain>
2020-06-03 00:07:52 +0900

    simpleshell 거의초기화

--- simpleshell.c
+++ simpleshell.c
@@ -7,8 +7,12 @@
 #define DELIMETER " \n"
 #define ARG_MAX 2097152
 
-void showPrompt(void);
-int checkBackground(char *);
+typedef struct _redirMode
+{  
+    int indir;
+    int outdir;
+    int errdir;
+}redirMode;
 
 int checkBackground(char *argv_list)
 {
@@ -26,6 +30,49 @@ int checkPipe(char *argv_list)
         return 1;
     }
     return 0;
+}
+
+redirMode checkRedirectionMode(char *argv_list)
+{
+    redirMode mode = {0,0,0};
+    if(strchr(argv_list,'<')!=NULL)
+    {
+        mode.indir = 1;
+    }
+    if(strchr(argv_list,'>')!=NULL)
+    {
+        mode.outdir = 1;
+    }
+    if(strstr(argv_list,"2>")!=NULL)
+    {
+        mode.errdir = 1;
+    }
+    return mode;
+}
+
+size_t parseIndir(char* buf, char ** arg_list,char * delimeter,char *input)
+{
+    size_t n = 0;
+    char * arg = strtok(buf, delimeter);
+
+    while(arg != NULL)
+    {
+        if(strchr(arg,'&')!=NULL)
+        {
+            continue;
+        }
+        else if(strchr(arg,'<')!=NULL)
+        {
+            input = arg;
+            continue;
+        }
+        arg_list[n++] = arg;
+        
+        arg = strtok(NULL, delimeter);
+    }
+    arg_list[n] = NULL;
+
+  return n;
 }
 
 size_t parse(char* buf, char ** arg_list,char * delimeter)
@@ -56,17 +103,35 @@ size_t parse(char* buf, char ** arg_list,char * delimeter)
   return n;
 }
 
-void execute(int isbg, char ** argv){
+void execute(int isbg, char ** argv,redirMode mode,char *input,char *output, char *err){
     // https://mhwan.tistory.com/42
     int status, pid;
-    if ((pid=fork()) == -1)
-        perror("fork failed");
-    else if(pid==0) {
-        if(execvp(*argv, argv)<0)
+    int input_fd,output_fd,err_fd;
+
+    if((pid=fork())==0) {
+        if(mode.indir)
         {
-            perror("Exec failed");
-            exit(1);
+            input_fd = open(input,O_RDONLY);
+            //indir
+            dup2(input_fd,STDIN_FILENO);
+            close(STDIN_FILENO);
+            close(input_fd);
         }
+        if(mode.outdir)
+        {
+            output_fd = open(output, O_CREAT|O_TRUNC|O_WRONLY, (int)384);
+            dup2(output_fd, STDOUT_FILENO);
+            close(STDOUT_FILENO);
+            close(input_fd);
+        }
+        if(mode.errdir)
+        {
+            err_fd = open(err, O_CREAT|O_TRUNC|O_WRONLY, (int)384);
+            dup2(err_fd,STDERR_FILENO);
+            close(STDERR_FILENO);
+            close(err_fd);
+        }
+        execvp(argv[0],argv);
     }
     else if (pid != 0) {
         if(isbg==0)
@@ -78,111 +143,39 @@ void execute(int isbg, char ** argv){
     } 
 }
 
-// 아래함수는 수정꼮해야함!!!!!!!!!!!!!!
-void executeRedir(struct modes * mode, char **argv, char* in, char * out){
-   
-}
-
-
-void executePipe(int isbg, char * argv1, char * argv2)
+void execPipes(int isbg,char ** pipe_list,int nCmd)
 {
-    int fd[2];
-    int status;
-    pid_t pid;
-    if(pipe(fd) == -1) {
-        perror("Pipe failed");
-        exit(1);
-    }
-
-    if((pid = fork()) == 0){            
-        close(STDOUT_FILENO);  
-        dup2(fd[1], 1);        
-        close(fd[0]);    
-        close(fd[1]);
-
-        execvp(argv1[0], argv1);
-    }
-
-    if((pid = fork()) == 0) {           
-        close(STDIN_FILENO); 
-        dup2(fd[0], 0);      
-        close(fd[1]);  
-        close(fd[0]);
-
-        execvp(argv2[0], argv2);
-    }
-    else
-    {
-        close(fd[0]); // 애매
-        close(fd[1]);
-        if(isbg==0)
-        {
-            wait(&status);
-        }
-        else 
-        {
-            waitpid(pid, &status, WNOHANG);    // 이게 맞나싶다. 계속 자식 프로세스 기다리는거같은데;
-        }
-    }
+    int fd[2] = {0,0};
+    char **pip1;
+    char **pip2;
+    pip1 = (char **)malloc(sizeof(char *) * (1024));
+    pip2 = (char **)malloc(sizeof(char *) * (1024));
+    char *inputDir;
+    int i=0;
+    redirMode mode={0,0,0};
+    int in;
     
-}
-int
-spawn_proc (int in, int out, char ** cmd)
-{
-  pid_t pid;
-
-  if ((pid = fork ()) == 0)
+    for(i=0; i<nCmd-1; i+=2)
     {
-      if (in != 0)
+        pipe(fd);
+        if(i==0)
         {
-          dup2 (in, 0);
-          close (in);
+            if(checkIndir(pipe_list[i]))
+            {
+                parseIndir(pipe_list[i],pip1,DELIMETER,inputDir);
+                mode.indir = 1;
+                execute(isbg,pip1,mode,inputDir,NULL,NULL);
+            }
+            else
+            {
+                parse(pipe_list[i],pip1,DELIMETER);
+                parse(pipe_list[i+1],pip2,DELIMETER);
+            }
+            
         }
 
-      if (out != 1)
-        {
-          dup2 (out, 1);
-          close (out);
-        }
-
-      return execvp (cmd[0], cmd);
     }
-
-  return pid;
 }
-void executePipeChain(int nPipe, char ** argv_list)
-{
-  int i;
-  pid_t pid;
-  int in, fd [2];
-
-  /* The first process should get its input from the original file descriptor 0.  */
-  in = 0;
-
-  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
-  for (i = 0; i < nPipe - 1; ++i)
-    {
-      pipe (fd);
-
-      /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
-      spawn_proc (in, fd [1], cmd + i);
-
-      /* No need for the write end of the pipe, the child will write here.  */
-      close (fd [1]);
-
-      /* Keep the read end of the pipe, the next child will read from there.  */
-      in = fd [0];
-    }
-
-  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
-     and output to the original file descriptor 1. */  
-  if (in != 0)
-    dup2 (in, 0);
-
-  /* Execute the last stage with the current process. */
-  return execvp (cmd [i].argv [0], (char * const *)cmd [i].argv);
-}
-
 
 void showPrompt(){
     printf("$");
@@ -190,43 +183,30 @@ void showPrompt(){
 
 
 
-int checkRedir(char * argv_list, char **inDir, char *outDir,char *errDir,int * dirMode)
-{
-   
-}
-
 int main(int argc, char * argv[]){
 
-    char ** argv_list;
-    argv_list = (char **)malloc(sizeof(char *) * (ARG_MAX));
-    char ** cmd_list;
+    char *** argv_list; // ""cmd","option","option"",""cmd","option","option""
+    argv_list = (char ***)malloc(sizeof(char **) * (ARG_MAX));
+    char ** cmd_list; // ; 단위로 구분한 명령어 집합
     cmd_list = (char **)malloc(sizeof(char *) * (ARG_MAX));
-    char ** pipe_list;
+    char ** pipe_list; // 파이프 단위로 쪼갠 명령어 집합 "cmd option","cmd option" , ...
     pipe_list = (char **)malloc(sizeof(char *) * (ARG_MAX));
-    char **pip1;
-    char **pip2;
-    pip1 = (char **)malloc(sizeof(char *) * (1024));
-    pip2 = (char **)malloc(sizeof(char *) * (1024));
+    
+    
     char buf[1024]; // for fgets 
 
     // for redirection 기록
     char ** inDir;
     inDir = (char **)malloc(sizeof(char *) * (1024));
-    char outDir[1024];
-    char errDir[1024];
 
-    
-
-    outDir[0] = "\0";
-    errDir[0] = "\0";
-    inDir[0][0] = "\0";
     int isInDirPipe = 0;
-    int redirMode = 0;
-    int isPipe = 0;
+    
     int i=1; int j=1;
+    int isPipe = 0;
     int isbg = 0;
+    redirMode mode = {0,0,0};
     size_t nCmd = 1;
-    size_t nPipe = 0;
+    size_t nPipe_cmd = 0;
     if(argc>1) // 인자 주어지는 경우. 아직 미완성. 인자없는 경우 완성하고 진행할것.
     {
         for(i=1; i< argc; i++)
@@ -243,13 +223,12 @@ int main(int argc, char * argv[]){
         showPrompt();
         if(!fgets(buf,sizeof(buf)-1,stdin)) // EOF 입력시 종료
         {
-            _exit(0);
+            exit(0);
         }
         if(strcmp(buf,"\n")==0) // 개행 입력 경우 다시..
         {
             continue;
         }
-
         if(strchr(buf,';')!=NULL) // ';' 있다는건 명령이 여러개라는 것
         {
            nCmd = parse(buf,cmd_list,";");
@@ -261,11 +240,34 @@ int main(int argc, char * argv[]){
         }
         for(i=0; i<nCmd; i++)
         {
-            nPipe = parse(cmd_list[i],pipe_list,"|");
-            for(j=0;j<nPipe-1;j+=2)
+            isbg = checkBackground(cmd_list[i]);
+            isPipe = checkPipe(cmd_list[i]);
+            mode = checkRedirectionMode(cmd_list[i]);
+            if(isPipe)
             {
-                parse
+                // pipe
+                if(mode.indir || mode.outdir || mode.errdir)
+                {
+
+                }
             }
+            else // no pipe
+            {
+                if(mode.indir || mode.outdir || mode.errdir)
+                {
+
+                }
+                else
+                {
+                    // normal exec
+                }
+                
+            }
+            /* 해야되는거: cmd_list 를 3차원의 argv_list 나 pipe_list 이용해서 파싱
+                해당 경우에 따라 execute 함수 만들기 with isbg...
+                작은거부터 차근차근해보자...
+                
+            */
         }
         
     }
